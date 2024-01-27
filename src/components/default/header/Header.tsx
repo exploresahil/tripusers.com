280763efb37f355554911787d7d18e941ce14eb5
Sahil Satpute <explore.sahil1@gmail.com>
2024-01-27 23:15:49 +0530

    updated header scss

--- src/components/default/header/Header.tsx
+++ src/components/default/header/Header.tsx
@@ -1,19 +1,50 @@
+"use client";
+
 import "./style.scss";
 import { getBrand } from "@/src/sanity/sanity-utils";
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "./Menu";
+import { brand } from "@/src/types/brand";
+import { useEffect, useState } from "react";
 
-const Header = async () => {
-  const brand = await getBrand();
-  const logo = brand[0].headerImage;
-  const logoMark = brand[0].logoMark;
+const Header = () => {
+  const [brandData, setBrandData] = useState<brand[]>([]);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    async function fetchBrand() {
+      const brand = await getBrand();
+      setBrandData(brand);
+    }
+    fetchBrand();
+  }, []);
+
+  const logo = brandData[0]?.headerImage;
+  const logoMark = brandData[0]?.logoMark;
+  const darkLogo = brandData[0]?.darkImage;
+
+  const handleScroll = () => {
+    // Check if the user has scrolled down enough to trigger the change
+    const scrolled = window.scrollY > 0;
+    setIsScrolled(scrolled);
+  };
+
+  useEffect(() => {
+    // Add scroll event listener when the component mounts
+    window.addEventListener("scroll", handleScroll);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <header>
+    <header className={isScrolled ? "scrolled" : ""}>
       <Link href="/" className="logo-container">
         <Image
-          src={logo}
+          src={isScrolled ? darkLogo : logo}
           alt="tripusers.com logo"
           fill
           sizes="(max-width: 768px) 600px, (max-width: 1200px) 1000px, 2000px"
