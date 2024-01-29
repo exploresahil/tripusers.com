54a23a9d29bd4350255a7043b87fc107e336f4c5
paragkoche <paragkoche0@gmail.com>
2024-01-29 18:55:52 +0530

    update wild life type update

---
+++ src/types/wildlife.ts
@@ -0,0 +1,47 @@
+import { PortableTextBlock } from "sanity";
+export type wildlifePackage = {
+  _id: string;
+  _createdAt: Date;
+  title: string;
+  slug: {
+    current: string;
+  };
+  packageImages: {
+    _id: string;
+    url: string;
+  }[];
+  timeline: string;
+  deal: string;
+  price: number;
+  priceSubtitle: string;
+  aboutTheTour: PortableTextBlock[];
+  itinerary: {
+    title: string;
+    day: number;
+    description: string;
+    content: {
+      title: string;
+      description: PortableTextBlock[];
+      images: {
+        _id: string;
+        url: string;
+      }[];
+    }[];
+  }[];
+};
+
+export type wildLife = {
+  _id: string;
+  _createdAt: Date;
+  name: string;
+  slug: {
+    current: string;
+  };
+  cardImage: string;
+  isTrending: boolean;
+  bannerImages: {
+    _id: string;
+    url: string;
+  }[];
+  wildlifePackage: wildlifePackage[];
+};
