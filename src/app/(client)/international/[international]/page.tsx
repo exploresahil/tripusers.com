c5aaa55d2ff2930228794455a10c47742f2dffdb
Sahil Satpute <explore.sahil1@gmail.com>
2024-02-10 17:14:57 +0530

    updated packages

--- src/app/(client)/international/[international]/page.tsx
+++ src/app/(client)/international/[international]/page.tsx
@@ -110,18 +110,19 @@ const page = ({ params }: Props) => {
                       allowTouchMove={false}
                       slidesPerView={1}
                     >
-                      {item.packageImages.map((item, index) => (
-                        <SwiperSlide key={index} className="swiperSlide-card">
-                          <div className="bg-container">
-                            <Image
-                              src={item.url}
-                              alt="hero background"
-                              fill
-                              sizes="(max-width: 768px) 600px, (max-width: 1200px) 1000px, 2000px"
-                            />
-                          </div>
-                        </SwiperSlide>
-                      ))}
+                      {item.packageImages &&
+                        item.packageImages.map((item, index) => (
+                          <SwiperSlide key={index} className="swiperSlide-card">
+                            <div className="bg-container">
+                              <Image
+                                src={item.url}
+                                alt="hero background"
+                                fill
+                                sizes="(max-width: 768px) 600px, (max-width: 1200px) 1000px, 2000px"
+                              />
+                            </div>
+                          </SwiperSlide>
+                        ))}
                     </Swiper>
                   </div>
                   <div className="text-container">
--- src/app/(client)/wild-life/[wildLife]/[wildLifePackage]/page.tsx
+++ src/app/(client)/wild-life/[wildLife]/[wildLifePackage]/page.tsx
@@ -87,21 +87,22 @@ const page = ({ params }: Props) => {
             allowTouchMove={false}
             slidesPerView={1}
           >
-            {data.packageImages.map((item, index) => (
-              <SwiperSlide key={index} className="swiperSlide-card">
-                <div className="bg-container">
-                  <div className="bg" />
-                  {item.url && (
-                    <Image
-                      src={item.url}
-                      alt="hero background"
-                      fill
-                      sizes="(max-width: 768px) 600px, (max-width: 1200px) 1000px, 2000px"
-                    />
-                  )}
-                </div>
-              </SwiperSlide>
-            ))}
+            {data.packageImages &&
+              data.packageImages.map((item, index) => (
+                <SwiperSlide key={index} className="swiperSlide-card">
+                  <div className="bg-container">
+                    <div className="bg" />
+                    {item.url && (
+                      <Image
+                        src={item.url}
+                        alt="hero background"
+                        fill
+                        sizes="(max-width: 768px) 600px, (max-width: 1200px) 1000px, 2000px"
+                      />
+                    )}
+                  </div>
+                </SwiperSlide>
+              ))}
           </Swiper>
           <div className="text-container">
             <p>Your unforgettable trip</p>
@@ -164,38 +165,39 @@ const page = ({ params }: Props) => {
             <div className="itinerary">
               <h3 className="itinerary-title">Itinerary</h3>
               <Accordion className="itinerary-accordion">
-                {data.itinerary.map((item, index) => (
-                  <AccordionItem
-                    key={index}
-                    header={`Day ${item.day} `}
-                    title={item.title}
-                    initialEntered={index === 0}
-                  >
-                    <div className="desc">
-                      <PortableText value={item.description} />
-                    </div>
-                    {item.content.map((content, index) => (
-                      <div key={index} className="content">
-                        <h4>{content.title}</h4>
-                        <PortableText value={content.description} />
-                        {content.images && (
-                          <div className="images-container">
-                            {content.images.map((img, index) => (
-                              <div key={index} className="img-container">
-                                <Image
-                                  src={img.url}
-                                  alt="hero background"
-                                  fill
-                                  sizes="(max-width: 768px) 600px, (max-width: 1200px) 1000px, 2000px"
-                                />
-                              </div>
-                            ))}
-                          </div>
-                        )}
+                {data.itinerary &&
+                  data.itinerary.map((item, index) => (
+                    <AccordionItem
+                      key={index}
+                      header={`Day ${item.day} `}
+                      title={item.title}
+                      initialEntered={index === 0}
+                    >
+                      <div className="desc">
+                        <PortableText value={item.description} />
                       </div>
-                    ))}
-                  </AccordionItem>
-                ))}
+                      {item.content.map((content, index) => (
+                        <div key={index} className="content">
+                          <h4>{content.title}</h4>
+                          <PortableText value={content.description} />
+                          {content.images && (
+                            <div className="images-container">
+                              {content.images.map((img, index) => (
+                                <div key={index} className="img-container">
+                                  <Image
+                                    src={img.url}
+                                    alt="hero background"
+                                    fill
+                                    sizes="(max-width: 768px) 600px, (max-width: 1200px) 1000px, 2000px"
+                                  />
+                                </div>
+                              ))}
+                            </div>
+                          )}
+                        </div>
+                      ))}
+                    </AccordionItem>
+                  ))}
               </Accordion>
             </div>
           </div>
