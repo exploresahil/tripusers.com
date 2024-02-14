ba5c2ad46012338a1ebe0cc5b4da4041422ee81e
paragkoche <paragkoche0@gmail.com>
2024-02-14 20:12:46 +0530

    update

--- src/components/home/testimonials/Testimonials.tsx
+++ src/components/home/testimonials/Testimonials.tsx
@@ -8,16 +8,35 @@ import {
   getTrending,
   getTrendingTestimonials,
 } from "@/src/sanity/sanity-utils";
-import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
+import { SwiperRef, SwiperSlide } from "swiper/react";
 import { Navigation, Autoplay, EffectCards, EffectFade } from "swiper/modules";
 import Link from "next/link";
 import Image from "next/image";
-
+import Swiper from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/effect-cards";
 import Testimonial from "@/src/types/testimonials";
+function getRandomUniqueElements(array: any[], count: number) {
+  // Shuffle the input array
+  const shuffledArray = array.slice().sort(() => Math.random() - 0.5);
 
+  // Ensure count does not exceed array length
+  count = Math.min(count, shuffledArray.length);
+
+  // Select unique elements
+  const result = [];
+  const selectedIndices = new Set();
+  while (result.length < count) {
+    const randomIndex = Math.floor(Math.random() * shuffledArray.length);
+    if (!selectedIndices.has(randomIndex)) {
+      result.push(shuffledArray[randomIndex]);
+      selectedIndices.add(randomIndex);
+    }
+  }
+
+  return result;
+}
 const Testimonials = () => {
   const [trending, setTrending] = useState<trending>();
   const [trendingTestimonial, setTrendingTestimonial] = useState<Testimonial[]>(
@@ -27,6 +46,9 @@ const Testimonials = () => {
   const prevRef = useRef(null);
   const nextRef = useRef(null);
 
+  const SwiperImageRef = useRef<any>();
+  const SwiperTextRef = useRef<any>();
+
   const fetchTrendingData = async () => {
     const trendingData = await getTrending();
     setTrending(trendingData);
@@ -34,27 +56,67 @@ const Testimonials = () => {
 
   const fetchTrendingTestimonialData = async () => {
     const trendingTestimonialData = await getTrendingTestimonials();
-    setTrendingTestimonial(() => trendingTestimonialData.slice(1, 5));
+    setTrendingTestimonial(() =>
+      getRandomUniqueElements(trendingTestimonialData, 5)
+    );
   };
 
   useEffect(() => {
     fetchTrendingData();
     fetchTrendingTestimonialData();
   }, []);
-  // const ImageSwiperRef = useRef<SwiperRef>(null);
-  // const TextSwiperRef = useRef<SwiperRef>(null);
-  // const [SlideIndex, setSlideIndex] = useState(0);
-  // console.log("SlideIndex -> ", SlideIndex);
-  // useEffect(() => {
-  //   console.log(
-  //     ImageSwiperRef.current?.swiper.activeIndex,
-  //     TextSwiperRef?.current?.swiper.activeIndex
-  //   );
+  console.log(trendingTestimonial);
 
-  //   TextSwiperRef?.current?.swiper?.slideTo(
-  //     ImageSwiperRef.current?.swiper.activeIndex || 0
-  //   );
-  // }, [SlideIndex]);
+  useEffect(() => {
+    if (SwiperImageRef.current && SwiperTextRef.current) {
+      let ImageSwiper = new Swiper(SwiperImageRef.current, {
+        initialSlide: 0,
+        effect: "cards",
+        autoplay: {
+          delay: 4000,
+          disableOnInteraction: false,
+        },
+        centeredSlides: true,
+        navigation: {
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        },
+        loop: true,
+        modules: [Navigation, Autoplay, EffectCards],
+        speed: 500,
+        allowTouchMove: false,
+        slidesPerView: "auto",
+
+        cardsEffect: {
+          rotate: false,
+          slideShadows: false,
+        },
+      });
+      let TextSwiper = new Swiper(SwiperTextRef.current, {
+        initialSlide: 5,
+        effect: "fade",
+        autoplay: {
+          delay: 4000,
+          disableOnInteraction: false,
+        },
+        centeredSlides: true,
+        loop: true,
+        modules: [Autoplay, EffectFade, Navigation],
+        speed: 500,
+        allowTouchMove: false,
+        slidesPerView: "auto",
+        navigation: {
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        },
+      });
+
+      return () => {
+        ImageSwiper.destroy();
+        TextSwiper.destroy();
+      };
+    }
+  }, [SwiperImageRef.current, SwiperTextRef.current]);
   //console.log("fetchTrending ->", trending);
   //console.log("fetchTrendingTestimonial ->", trendingTestimonial);
 
@@ -69,85 +131,50 @@ const Testimonials = () => {
       <div className="testimonials-container">
         {trendingTestimonial.length > 0 && (
           <>
-            <Swiper
-              // ref={ImageSwiperRef}
-              initialSlide={0}
-              effect={"cards"}
-              autoplay={{
-                delay: 4000,
-                disableOnInteraction: false,
-              }}
-              centeredSlides={true}
-              navigation={{
-                prevEl: prevRef.current,
-                nextEl: nextRef.current,
-              }}
-              loop={true}
-              modules={[Navigation, Autoplay, EffectCards]}
-              className="mySwiper-images"
-              speed={500}
-              allowTouchMove={false}
-              slidesPerView={"auto"}
-              cardsEffect={{
-                rotate: false,
-                slideShadows: false,
-              }}
-              // onSlideChange={(swiper) => {
-              //   setSlideIndex(swiper.realIndex);
-              // }}
+            <div
+              ref={SwiperImageRef}
+              className="mySwiper-images swiper-container"
             >
-              {trendingTestimonial.map((data, index) => (
-                <SwiperSlide key={index} className="swiperSlide-card">
-                  <Link href="#">
-                    <Image
-                      src={data?.cardImage}
-                      alt="hero background"
-                      fill
-                      sizes="(max-width: 768px) 600px, (max-width: 1200px) 1000px, 2000px"
-                    />
-                  </Link>
-                </SwiperSlide>
-              ))}
+              <div className="swiper-wrapper">
+                {trendingTestimonial.map((data, index) => (
+                  <div key={index} className="swiper-slide swiperSlide-card">
+                    <Link href="#">
+                      <Image
+                        src={data?.cardImage}
+                        alt="hero background"
+                        fill
+                        sizes="(max-width: 768px) 600px, (max-width: 1200px) 1000px, 2000px"
+                      />
+                    </Link>
+                  </div>
+                ))}
+              </div>
               <div className="prev" ref={prevRef}>
                 prev
               </div>
               <div className="next" ref={nextRef}>
                 next
               </div>
-            </Swiper>
-            <Swiper
-              // ref={TextSwiperRef}
-              initialSlide={3}
-              effect={"fade"}
-              autoplay={{
-                delay: 4000,
-                disableOnInteraction: false,
-              }}
-              centeredSlides={true}
-              loop={true}
-              modules={[Autoplay, EffectFade, Navigation]}
-              className="mySwiper-content"
-              speed={500}
-              allowTouchMove={false}
-              slidesPerView={"auto"}
-              navigation={{
-                prevEl: prevRef.current,
-                nextEl: nextRef.current,
-              }}
+            </div>
+            <div
+              ref={SwiperTextRef}
+              className="mySwiper-content swiper-container"
             >
-              {trendingTestimonial.map((data, index) => (
-                <SwiperSlide key={index} className="swiperSlide-card">
-                  <Link href="#">
-                    <h3>{data?.title}</h3>
-                  </Link>
-                  <div className="hashtags">
-                    {data.hashtags?.map((data, index) => (
-                      <p key={index}>#{data.name}</p>
-                    ))}
+              <div className="swiper-wrapper">
+                {trendingTestimonial.map((data, index) => (
+                  <div key={index} className="swiper-slide swiperSlide-card">
+                    <Link href="#">
+                      <h3>{data?.title}</h3>
+                    </Link>
+                    <div className="hashtags">
+                      {data.hashtags?.map((data, index) => (
+                        <p key={index}>#{data.name}</p>
+                      ))}
+                    </div>
                   </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
+                ))}
+              </div>
+            </div>
           </>
         )}
       </div>
